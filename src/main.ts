7fe54400e7f94a9700a2cf0ff971c8bc3dcfb535
Alex Bostock <alex@bostock.uk>
2023-12-05 20:47:19 +0000

    feat: implement day 2: rock paper scissors

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { open } from 'node:fs/promises';
 import * as day1 from './lib/1-calorie-counting.js';
+import * as day2 from './lib/2-rock-paper-scissors.js';
 
 const puzzleKey = process.argv[2];
 
@@ -8,6 +9,8 @@ const [day] = puzzleKey.split('.');
 const puzzleMap: Record<string, (input: string) => unknown> = {
   '1.1': day1.part1,
   '1.2': day1.part2,
+  '2.1': day2.part1,
+  '2.2': day2.part2,
 };
 
 const puzzle = puzzleMap[puzzleKey];
